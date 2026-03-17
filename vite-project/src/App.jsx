3e6861e35a3d0acdd70813e75b8e6bfb14c33953
Hafsa-Belahnech <250507787+Hafsa-Belahnech@users.noreply.github.com>
2026-03-17 21:47:51 +0000

    fix

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -9,13 +9,13 @@ function App() {
   return (
     <div>
       <h1>TP Hooks Avancés – Niveau Débutant</h1>
-      <h2>Compteur avec useReducer</h2>
+      <h3>C'est le compteur avec useReducer</h3>
       <Compteur />
-      <h2>Mettre le focus avec useRef</h2>
+      <h2>Mettre le focus avec useRef -> </h2>
       <FocusInput />
       <h2>Compteur de rendus</h2>
       <CompteurRendu />
-      <h2>Liste d’articles avec useFetch</h2>
+      <h2>La liste d’articles avec useFetch</h2>
       <ListeArticles />
       <h2>Timer avec nettoyage d’effet</h2>
       <Timer />
